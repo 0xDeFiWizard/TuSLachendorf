4db418f319d9ec2ec681243872ae55894293a748
0xDeFiWizard <maurice.mueller1708@icloud.com>
2026-03-05 19:55:13 +0100

    Update service worker cache and asset paths

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'vereinskasse-v1';
-const ASSETS = ['/', '/index.html'];
+const CACHE = 'tus-lachendorf-v1';
+const ASSETS = ['/TuSLachendorf/', '/TuSLachendorf/index.html'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -15,6 +15,6 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/index.html')))
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('/TuSLachendorf/index.html')))
   );
 });
